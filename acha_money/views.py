452749f41c91ef9e2e58d0e76b57a3a4ae316289
Nok5051 <nok5051@naver.com>
2022-09-13 14:02:34 +0900

    sjgwak 2 upload

--- acha_money/views.py
+++ acha_money/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def money_index(request):
-    return render(request, 'acha_money/money_index.html')
+    return render(request, 'acha_money/test.html')
 
