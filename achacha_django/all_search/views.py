6185d14c695c6a9ad73b5da21fa9609cf79757dc
suranchoi <tnfks0217@gmail.com>
2022-09-20 13:08:10 +0900

    acha_money

--- achacha_django/all_search/views.py
+++ achacha_django/all_search/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from . models import LostItems
+from hdfs import InsecureClient
 # Create your views here.
 
 def all_index(request):
-    return render(request, 'all_search/all_index.html')
+    lost_items_list = LostItems.objects.all()
+
+    paginator = Paginator(lost_items_list, 5)
+    page = request.GET.get('page')
+
+    posts = paginator.get_page(page)
+
+    client = InsecureClient('http://localhost:50070/')
+    with client.read('/user/service_img/')
+    return render(request, 'all_search/all_index.html', {'lost_items_list': lost_items_list,
+                                                         'posts': posts})
