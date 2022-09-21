f7abece14bdbb7e00e476e9e1fbb77ea9220a5ce
jooin1785 <jooin1785@naver.com>
2022-09-21 17:10:08 +0900

    'all_search'

--- achacha_django/all_search/urls.py
+++ achacha_django/all_search/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_index),
+    path('all_detail/<int:lost_items_id_pk>/', views.all_detail, name="all_detail")
 ]
 
 
--- achacha_django/all_search/views.py
+++ achacha_django/all_search/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from . models import LostItems
 from hdfs import InsecureClient
@@ -18,3 +18,9 @@ def all_index(request):
     client = InsecureClient('http://localhost:50070/')
     return render(request, 'all_search/all_index.html', {'lost_items_list': lost_items_list,
                                                          'posts': posts})
+
+def all_detail(request, lost_items_id_pk):
+    lost_items_list= LostItems.objects.filter(lost_items_id_pk=lost_items_id_pk)
+    return render(request, 'all_search/all_detail.html', {'lost_items_list': lost_items_list})
+
+
