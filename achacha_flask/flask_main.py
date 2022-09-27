9d35d40b846c4ea65be214594b8d5571820ca9d3
seongminlee812 <sungminlee32@gmail.com>
2022-09-27 15:02:43 +0900

    flask 수정 및 vector_frame 수정

--- achacha_flask/flask_main.py
+++ achacha_flask/flask_main.py
@@ -23,40 +23,54 @@ from scipy.spatial import distance
 import torch
 import glob
 import keras
-# from yolo.yolo_prototype import Yolo
+
 
 # 경고끄기 (option)
 import warnings
-
 warnings.filterwarnings('ignore')
 
 # %%
 
-app = Flask(__name__)
-
+application = Flask(__name__)
 
 # DS
 
 
-# %%
+#%%
 
-# 욜로 함수 제외
 # def crop(input_file):
+#     """
 #     im = Image.open(input_file).convert('RGB')
 #     result = yolo_model(im)
 #     result_df = result.pandas().xyxy[0]
 #     name = result.pandas().xyxy[0]['name'][0]
-#     im = im.crop((result_df['xmin'], result_df['ymin'], result_df['xmax'], result_df['ymax']))
-#     # crop 이미지 저장 코드
-#     # im.save(f'/home/ubuntu/WEB_SERVICE_ACHACHA/FLASK/yolo/uploaded_file/real_{category}_pred_{name}_{time.strftime("%H_%M_%S")}.jpg')
-#     return im
-
-
-# %%
+#     im = im.crop( (result_df['xmin'], result_df['ymin'], result_df['xmax'], result_df['ymax']) )
+#     im.save(f'/home/ubuntu/WEB_SERVICE_ACHACHA/FLASK/yolo/uploaded_file_crop/real_{category}_pred_{name}_{time.strftime("%H_%M_%S")}.jpg')
+#     """
+#
+#     im_raw = Image.open(input_file).convert('RGB')
+#     result = yolo_model(im_raw)
+#     result_df = result.pandas().xyxy[0]
+#     print(result_df)
+#     name = result.pandas().xyxy[0]['name'][0]
+#     im_cropped = im_raw.crop((result_df['xmin'], result_df['ymin'], result_df['xmax'], result_df['ymax']))
+#     im_cropped.save(f'/home/ubuntu/WEB_SERVICE_ACHACHA/FLASK/yolo/uploaded_file_crop/real_{category}_pred_{name}_{time.strftime("%H_%M_%S")}.jpg')
+#
+#     im_raw_filename = f'real_{category}_pred_{name}_{time.strftime("%H_%M_%S")}'
+#     im_raw.save('/home/ubuntu/WEB_SERVICE_ACHACHA/FLASK/yolo/uploaded_file_raw/' + im_raw_filename + '.jpg')
+#     tmp_text = str(result.pandas().xyxy[0][['name', 'xmin', 'ymin', 'xmax', 'ymax']].loc[0].values)
+#
+#     f = open('/home/ubuntu/WEB_SERVICE_ACHACHA/FLASK/yolo/label_text/' + im_raw_filename + '.txt', 'w')
+#     f.write(tmp_text)
+#     f.close()
+#
+#     return im_cropped
+    
+#%%
 
 def extract(file):
     # global model
-    file = file.convert('RGB').resize((224, 224))
+    file = Image.open(file).convert('RGB').resize((224, 224))
     file = np.array(file) / 255.0  # 정규화
 
     embedding = model.predict(file[np.newaxis, ...])
@@ -65,7 +79,6 @@ def extract(file):
 
     return flattened_feature
 
-
 # %%
 
 def get_dataframe(category):
@@ -73,7 +86,6 @@ def get_dataframe(category):
     tmp_output = np.load(output_path + f'{category}_output({model_name}).npy', allow_pickle=True)
     df = pd.DataFrame({'filename': tmp_filename, 'output': tmp_output})
     return df
-
 
 # %%
 
@@ -96,12 +108,13 @@ def search_img(category, cropped_file, threshold=0.4):
 
     return df_top_sim.filename.values
 
-
 # %%
-@app.route('/', methods=["GET", "POST"])
+#@application.route('/')
+#@application.route('/flask/', methods=["GET","POST"])
+@application.route('/', methods=["GET", "POST"])
 def get_data():
     if request.method == "POST":
-        data = request.form  # MultiDict 형태
+        data = request.form #MultiDict 형태
 
         image_str = data['image']
         global category
@@ -111,16 +124,16 @@ def get_data():
         image_bytes = bytes(image_str, 'utf-8')
 
         decoded_string = io.BytesIO(base64.b64decode(image_bytes))
-
-        # --------------------------------------------------------------------------
+        
+    #--------------------------------------------------------------------------
         # modeling
 
         # yolo
-        yolo_start_time = time.time()
-        global yolo_model
-        img = crop(decoded_string)
-        print(f'yolo소요시간 : {time.time() - yolo_start_time}초')
-
+        # yolo_start_time = time.time()
+        # global yolo_model
+        # img = crop(decoded_string)
+        # print(f'yolo소요시간 : {time.time()-yolo_start_time}초')
+        
         global model_category
         global model, model_name
         model = model_category.model_dict[category]['model']
@@ -128,42 +141,40 @@ def get_data():
         threshold = model_category.model_dict[category]['threshold']
 
         start_time = time.time()
-
+        
         global output_path
         output_path = f"./vector_frame_95/{category}/"
-
-        result = search_img(category, img, threshold=threshold)
-
+        
+        global result
+        result = search_img(category,  decoded_string, threshold=threshold)
+        
         result = result.astype('str')
         result = json.dumps(result.tolist())
-
+    
+        
         print(f'모델 소요시간 : {time.time() - start_time:.3f}초')  # 테스트용 코드 -> 추후 삭제
 
-        print(f'총 소요시간 : {time.time() - yolo_start_time:.3f}초')
         print(result)
         print(len(result))
 
     return result
-
 
 if __name__ == "__main__":
     start_time = time.time()
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
     os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
-    #     gpu cash 지워주기
+#     gpu cash 지워주기
     import gc
-
     gc.collect()
-    torch.cuda.empty_cache()
-
-# 욜로 모델 제외
+    torch.cuda.empty_cac
+    #
     # yolo = Yolo()
     # yolo_model = yolo.load_model()
-
-    gc.collect()
-    torch.cuda.empty_cache()
-
+    # #
+    # gc.collect()
+    # torch.cuda.empty_cache()
+    
     model_category = ModelCategory()
     model_category.set_model()
 
@@ -171,6 +182,5 @@ if __name__ == "__main__":
     gc.collect()
     torch.cuda.empty_cache()
     try:
-        app.run(host="0.0.0.0", port=5001, debug=True, use_reloader=False)
-    except Exception as e:
-        print(e)
+        application.run(host="0.0.0.0", debug=True, use_reloader=False)
+    except Exception as e: print(e)
